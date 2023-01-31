94d837de1764b698e3d6b792b034df56fe324a1f
amriallam <98554346+amriallam@users.noreply.github.com>
2023-01-31 20:05:46 +0200

    authMiddleware Doctor Update

--- Middlewares/authenticationMW.js
+++ Middlewares/authenticationMW.js
@@ -1,38 +1,55 @@
-// const { response } = require('express');
-const jwt = require('jsonwebtoken');
+const { ChainCondition } = require("express-validator/src/context-items");
+const jwt = require("jsonwebtoken");
+const { get } = require("../Models/addressModel");
 
 module.exports = (request, response, next) => {
-  try{
+  try {
     const token = request.get("authorization").split(" ")[1];
-    const decodedToken = jwt.verify(token,  process.env.SECRET_KEY);
-    request.userData = { id:decodedToken.id, username: decodedToken.username, role: decodedToken.role };
-  }
-  catch (error) {
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    request.userData = {
+      id: decodedToken.id,
+      username: decodedToken.username,
+      role: decodedToken.role,
+    };
+  } catch (error) {
     error.status = 403;
     error.message = "You are not authorized";
     next(error);
   }
   next();
-}
-
-
+};
 module.exports.checkAdmin = (request, response, next) => {
   if (request.userData.role !== "admin") {
     const error = new Error("You are not authorized to access this resource");
     error.status = 403;
     next(error);
-  }
-  else{
+  } else {
     next();
   }
-}
+};
 module.exports.checkPatient = (request, response, next) => {
-  if (request.userData.role == "patient" && request.userData.id==request.params.id) {
-  next();
-  }
-  else{
-    // const error = new Error("You are not authorized to access this resource",request.userData.id,request.params.id);
-    // error.status = 403;
+  if (
+    request.userData.role == "admin" ||
+    (request.userData.role == "patient" &&
+      request.userData.id == request.params.id)
+  ) {
     next();
+  } else {
+    const error = new Error("You are not authorized to access this resource");
+    error.status = 403;
+    next(error);
   }
-}
+};
+module.exports.checkDoctor = (request, response, next) => {
+  if (
+    request.userData.role == "admin" ||
+    (request.userData.role == "doctor" &&
+      request.userData.id == request.params.id)
+  ) {
+    next();
+  } else {
+    const error = new Error("You are not authorized to access this resource");
+    error.status = 403;
+    next(error);
+  }
+};
