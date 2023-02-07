84cbf76d7f155b18a9c35729d4363dcc36837903
hassa <hassa@AHASSAN>
2023-02-07 09:09:45 +0200

    multerUpload

--- Controller/medicineController.js
+++ Controller/medicineController.js
@@ -2,6 +2,30 @@
 
 /* require all needed modules */
 const medicineSchema = require("../Models/medicineModel");
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './upload/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/jpg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 10
+  },
+  fileFilter: fileFilter
+});
+exports.uploadImage = upload.single('photo')
 
 /* require helper functions (filter,sort,slice,paginate) */
 const {
