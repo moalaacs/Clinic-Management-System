fc9728b705a91e78c7ee9a682c99acd1af1e7675
Amr Allam <amr.i.allam@hotmail.com>
2023-02-08 14:54:02 +0200

    authControllerFixed

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,63 +1,65 @@
 const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 // const Admin = require("./../Models/adminModel");
 // const Patient = require("../Models/patientModel");
 // const Employee = require("../Models/employeeModel");
 const users = require("../Models/usersModel");
 exports.login = async (request, response, next) => {
-  let token;
   try {
+    let token;
     const { email, password } = request.body;
     const foundUser = await users.findOne({
       _email: email,
-      _password: password,
     });
     if (foundUser) {
-      switch (foundUser._role) {
-        case "admin": {
-          token = jwt.sign(
-            {
-              role: "admin",
-              email,
-              id: foundUser._id,
-              forClinic: foundUser._forClinic,
-            },
-            process.env.SECRET_KEY,
-            { expiresIn: "1h" }
-          );
-          return response
-            .status(200)
-            .json({ message: "Admin login successful", token });
-        }
-        case "doctor": {
-          token = jwt.sign(
-            { role: "doctor", email, id: foundUser._id },
-            process.env.SECRET_KEY,
-            { expiresIn: "1h" }
-          );
-          return response
-            .status(200)
-            .json({ message: "Doctor login successful", token });
-        }
-        case "employee": {
-          token = jwt.sign(
-            { role: "employee", email, id: foundUser._id },
-            process.env.SECRET_KEY,
-            { expiresIn: "1h" }
-          );
-          return response
-            .status(200)
-            .json({ message: "Employee login successful", token });
-        }
-        case "patient": {
-          token = jwt.sign(
-            { role: "patient", email, id: foundUser._id },
-            process.env.SECRET_KEY,
-            { expiresIn: "1h" }
-          );
-          return response
-            .status(200)
-            .json({ message: "Patient login successful", token });
+      const isMatch = await bcrypt.compare(password, foundUser._password);
+      if (isMatch) {
+        switch (foundUser._role) {
+          case "admin": {
+            token = jwt.sign(
+              {
+                role: "admin",
+                email,
+                id: foundUser._id,
+                forClinic: foundUser._forClinic,
+              },
+              process.env.SECRET_KEY,
+              { expiresIn: "1h" }
+            );
+            return response
+              .status(200)
+              .json({ message: "Admin login successful", token });
+          }
+          case "doctor": {
+            token = jwt.sign(
+              { role: "doctor", email, id: foundUser._id },
+              process.env.SECRET_KEY,
+              { expiresIn: "1h" }
+            );
+            return response
+              .status(200)
+              .json({ message: "Doctor login successful", token });
+          }
+          case "employee": {
+            token = jwt.sign(
+              { role: "employee", email, id: foundUser._id },
+              process.env.SECRET_KEY,
+              { expiresIn: "1h" }
+            );
+            return response
+              .status(200)
+              .json({ message: "Employee login successful", token });
+          }
+          case "patient": {
+            token = jwt.sign(
+              { role: "patient", email, id: foundUser._id },
+              process.env.SECRET_KEY,
+              { expiresIn: "1h" }
+            );
+            return response
+              .status(200)
+              .json({ message: "Patient login successful", token });
+          }
         }
       }
     } else {
